5cda8f4aeaefd54ccf1cde513e69a2ce771ac47f
Pascal Strebel <pas3398@hotmail.ch>
2023-12-01 08:18:48 +0100

    solved day 1

--- day01/code.py
+++ day01/code.py
@@ -1,40 +1,125 @@
 import os.path
 
-input_file = os.path.join(os.path.dirname(__file__), 'sample_input_2.txt')
-
-def replace_spelled_digits(s):
-    s = s.replace("one", "1")
-    s = s.replace("two", "2")
-    s = s.replace("three", "3")
-    s = s.replace("four", "4")
-    s = s.replace("five", "5")
-    s = s.replace("six", "6")
-    s = s.replace("seven", "7")
-    s = s.replace("eight", "8")
-    s = s.replace("nine", "9")
-    return s
+input_file = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 def read_input():
     with open(input_file, 'r') as f:
         lines = f.readlines()
-        input = []
-        for line in lines:
-            line = replace_spelled_digits(line)
-            print(line)
-            line_numbers = []
-            for character in line:
-                if character.isdigit():
-                    line_numbers.append(int(character))
-            input.append(line_numbers)
-        print(input)
-        return input
+        return lines
 
-def compute():
+# --- Day 1: Trebuchet?! ---
+# Something is wrong with global snow production, and you've been selected to take a look. 
+# The Elves have even given you a map; on it, they've used stars to mark the top fifty 
+# locations that are likely to be having problems.
+
+# You've been doing this long enough to know that to restore snow operations, you need to 
+# check all fifty stars by December 25th.
+
+# Collect stars by solving puzzles. Two puzzles will be made available on each day in the 
+# Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle 
+# grants one star. Good luck!
+
+# You try to ask why they can't just use a weather machine ("not powerful enough") and where 
+# they're even sending you ("the sky") and why your map looks mostly blank ("you sure ask a 
+# lot of questions") and hang on did you just say the sky ("of course, where do you think 
+# snow comes from") when you realize that the Elves are already loading you into a trebuchet 
+# ("please hold still, we need to strap you in").
+
+# As they're making the final adjustments, they discover that their calibration document 
+# (your puzzle input) has been amended by a very young Elf who was apparently just excited 
+# to show off her art skills. Consequently, the Elves are having trouble reading the values 
+# on the document.
+
+# The newly-improved calibration document consists of lines of text; each line originally 
+# contained a specific calibration value that the Elves now need to recover. On each line, 
+# the calibration value can be found by combining the first digit and the last digit (in 
+# that order) to form a single two-digit number.
+
+# For example:
+
+# 1abc2
+# pqr3stu8vwx
+# a1b2c3d4e5f
+# treb7uchet
+# In this example, the calibration values of these four lines are 12, 38, 15, and 77. 
+# Adding these together produces 142.
+
+# Consider your entire calibration document. What is the sum of all of the calibration values?
+
+def compute_part_1():
     input = read_input()
     sum = 0
     for line in input:
-        sum += line[0]*10 + line[len(line)-1]
+        line_numbers = []
+        for character in line:
+            if character.isdigit():
+                line_numbers.append(int(character))
+        sum += line_numbers[0]*10 + line_numbers[len(line_numbers)-1]
     return sum
 
+# Your puzzle answer was 55447.
+
+# --- Part Two ---
+# Your calculation isn't quite right. It looks like some of the digits are actually 
+# spelled out with letters: one, two, three, four, five, six, seven, eight, and nine 
+# also count as valid "digits".
+
+# Equipped with this new information, you now need to find the real first and last digit 
+# on each line. For example:
+
+# two1nine
+# eightwothree
+# abcone2threexyz
+# xtwone3four
+# 4nineeightseven2
+# zoneight234
+# 7pqrstsixteen
+# In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. 
+# Adding these together produces 281.
+
+# What is the sum of all of the calibration values?
+
+DIGITS = {
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+}
+
+def replace_spelled_digits(s):
+    i = 0
+    new_s = ""
+    while(i < len(s)):
+        found_one = False
+        for digit in DIGITS:
+            if(s[i:i+len(digit[0])] == digit[0]):
+                new_s += digit[1]
+                found_one = True
+                break
+        if not found_one:
+            new_s += s[i]
+        i += 1 # The right calibration values for string "eighthree" is 83 and for "sevenine" is 79.
+    return new_s
+
+def compute_part_2():
+    input = read_input()
+    sum = 0
+    for line in input:
+        line = replace_spelled_digits(line)
+        line_numbers = []
+        for character in line:
+            if character.isdigit():
+                line_numbers.append(int(character))
+        sum += line_numbers[0]*10 + line_numbers[len(line_numbers)-1]
+    return sum
+
+# Your puzzle answer was 54706.
+
 if __name__ == '__main__':
-    print(compute())
+    print(f"PART 1: {compute_part_1()}")
+    print(f"PART 2: {compute_part_2()}")
