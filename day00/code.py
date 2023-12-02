707eb988cc5d996f08ec27459ed9dbb6c0d4c0e3
Pascal <pas3398@hotmail.ch>
2023-12-02 11:15:58 +0100

    update sample day template

--- day00/code.py
+++ day00/code.py
@@ -1,19 +1,33 @@
 import os.path
+import re
 
-input_file = os.path.join(os.path.dirname(__file__), 'input.txt')
 
-def read_input():
-    with open(input_file, 'r') as f:
+def read_input(input_file_name):
+    input_file = os.path.join(os.path.dirname(__file__), input_file_name)
+    with open(input_file, "r") as f:
         lines = f.readlines()
-        input = []
-        for line in lines:
-            input.append([int(num) for num in line.split()])
-        print(input)
-        return input
+        return lines
 
-def compute():
-    input = read_input()
+
+class Dummy:
+    def __init__(self):
+        self.dummy = 0
+
+    def setup(self, input):
+        for set in re.findall(r"[^;]+", input):
+            pass
+
+
+def compute_part_1(input_file_name="input.txt"):
+    input = read_input(input_file_name)
     return 0
 
-if __name__ == '__main__':
-    compute()
+
+def compute_part_2(input_file_name="input.txt"):
+    input = read_input(input_file_name)
+    return 0
+
+
+if __name__ == "__main__":
+    print(f"PART 1: {compute_part_1()}")
+    print(f"PART 2: {compute_part_2()}")
